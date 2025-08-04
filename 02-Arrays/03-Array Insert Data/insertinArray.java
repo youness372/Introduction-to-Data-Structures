60125881366aa4e2de8ce5962a64d17a6896f9d3
younsse347 <younessagrine2@gmail.com>
2025-08-04 19:46:03 +0100

    Create insertinArray.java

---
+++ 02-Arrays/03-Array Insert Data/insertinArray.java
@@ -0,0 +1,17 @@
+
+public class InsertInArray{   
+  public static void main(String[] args)  {      
+
+    int [] nums =  new int[5] ;   
+    nums[0] = 10 ;   
+    nums[1] = 20 ;   
+    nums[2] = 30 ;   
+    nums[3] = 40 ;   
+    nums[4] = 50 ;    
+
+    for (int i = 0  ;  i < nums.length; i++)  {     
+      System.out.println(nums[i]) ;   
+    }
+
+  } 
+}  
