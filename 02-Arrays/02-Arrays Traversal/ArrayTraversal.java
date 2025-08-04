53fdde4bf1ef2169e305c4b054d3391441582aea
younsse347 <younessagrine2@gmail.com>
2025-08-04 19:04:49 +0100

    Create ArrayTraversal.java

---
+++ 02-Arrays/02-Arrays Traversal/ArrayTraversal.java
@@ -0,0 +1,20 @@
+
+public class TraversalArray {   
+  public static void main (String[]  args) {    
+
+    int [] numbers =  new int[5] ;     
+    String[]  Straw_hat = {"youness", "Luffy", "zoro", "Franky" , "Nami", "Usopp", "chopper", "Sanji", "Rubin", "brook"} ;      
+    System.out.println ("Numbers list is") ;     
+    for (int i = 0  ; i < numbers.length ;  i++) {    
+          System.out.println(numbers[i]);  }   
+    
+    System.out.println("The Straw hat crew ");   
+
+    for (int i = 0; i < Straw_hat.length; i++) {   
+            System.out.println(Straw_hat[i]);  }   
+  }   
+} 
+
+
+
+    
