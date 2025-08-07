eb94bc32448005ae6eaf563f353f0895920c5592
younsse347 <younessagrine2@gmail.com>
2025-08-07 17:09:16 +0100

    Create 03-HasPairSuminsteadloop.py

---
+++ 02-Arrays/11-Array Question1/03-HasPairSuminsteadloop.py
@@ -0,0 +1,14 @@
+
+def hasPairSum(arr, size, Target) :
+  for i in range (len(arr)-1) :
+    for j in range (i+1 , len(arr)) :
+      if arr[i] + arr[j] == Target :
+          return  True
+  return False
+arr =  [0,-1,2,-3,1]
+size = len(arr)
+Target = -2
+if hasPairSum (arr , size , Target)  :
+    print("the sum of two elements in the array equal to Target is : " , Target)
+else :
+    print("the sum of two elements in the array equal to Target is : " ,Target)
